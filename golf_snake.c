1c07253470da663c9126bf3c507e3241ee079350
CuWO4 <wutong.tony@outlook.com>
2024-06-06 03:37:41 +0800

    feat: compress the code to 6 lines
    * it's possible to compress to 3 lines

--- golf_snake.c
+++ golf_snake.c
@@ -1,7 +1,6 @@
 int main() {
-  for(char d[80]={1,8,-1,-8},*m=d+8,h=52,l=4,v=3,w=v,c,*s="dsawDSAW|\n|%c`|%c",e=0,i,a=(srand((int)m),4);!e;Sleep(*s)){
-    for(system("cls");kbhit();)for(c=getch(),i=0;i<8;++i)v=s[i]==c&i+w?i%4:v;
-    if(!(e=m[h+=d[w=v]]))if(m[h]=l,!(e=l>63|h&192|!(v^2|h%8^7&&v|h%8))&h==a)for(++l;m[a=rand()%64];);
-    for(i=0;i<64;++i)m[i]-=!!m[i],printf(s+8+3*!!(i%8)+!i,"@ *"[!m[i]+(i==a)],i<63?8:0);
+  for(char s[99]="dkb[dsaw|\n|%c`|%c",*m=s+18,h=52,l=4,v=3,w=v,c,g=1,i,a=(srand((int)m),4);g;Sleep(*s)){
+    for(system("cls");kbhit();)for(c=getch(),i=3;++i<8;)v=s[i]==c&i+w?i%4:v;
+    if(g=!m[h+=s[w=v]-99],m[h]=l+=h==a,g&=l<65&!(h&192)&&v^2|h%8^7&&v|h%8)for(i=-1;++i<64;m[i]-=!!m[i],printf(s+8+3*!!(i%8)+!i,i^a?"@ "[!m[i]]:42,(i<63)*8))for(;m[a];a=rand()%64);
   }
 }
