e8a2eb662d912c2650a29c0a0088e6fde74a7767
CuWO4 <wutong.tony@outlook.com>
2024-09-02 23:08:53 +0800

    style: compress code

--- golf_snake.c
+++ golf_snake.c
@@ -1,3 +1,3 @@
 int main(){
-  for(char s[99]="dkb[dsaw|\n|%c`|%c",*m=s+27,h=52,l=4,v=3,w=v,c=2,g=1,i,a=(srand((int)m),4);g;Sleep(*s),c=2)for(system("cls");c;)for(c=kbhit()?getch():1,i=3;c^1?++i<8:c--;v=c&&s[i]==c&i+w?i%4:v)if(!c)for(g=!(m[h+=s[w=v]-99]|h&192)&&v^2|h%8^7&&v|h%8,m[h]=l+=h==a,i=0;i<64;m[i++]-=!!m[i])for(printf(s+8+3*!!(i%8)+!i,i^a?"@ "[!m[i]]:42,(i<63)*8);m[a];a=rand()%64);
+  for(char s[99]="0'.7dwas|\n|%c'|%c",*m=s+27,h=52,l=4,v=1,w=v,c=2,g=1,i,a=(srand((int)m),4);g;Sleep(99),c=2)for(system("cls");c;)for(c=kbhit()?getch():1,i=3;c^1?++i<8:c--;v=c&&s[i]==c&i+w?i%4:v)if(!c)for(g=!(m[h+=s[w=v]-47]|h&192)&&v^2|h%8^7&&v|h%8,m[h]=l+=h==a,i=0;i<64;m[i++]-=!!m[i])for(printf(s+8+3*!!(i%8)+!i,i^a?s[!m[i]]:42,(i<63)*8);m[a];a=rand()%64);
 }
