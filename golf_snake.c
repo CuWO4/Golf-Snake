90258b86527dda10cfc07daacefff543036a9c8c
CuWO4 <wutong.tony@outlook.com>
2025-06-24 14:40:21 +0800

    feat: remove all line breaking, change the target to the least used ASCII characters

--- golf_snake.c
+++ golf_snake.c
@@ -1,3 +1,1 @@
-int main(){
-  for(char s[99]="0'.7cu^o\e[H\n%c'",*m=s+27,h=52,l=3,v=1,w,c,g=0,i,a=(srand((int)m),4);!g;Sleep(99))for(c=2;c>0;i=3)for(c=kbhit()?getch():1;c--&&++i<8;v=c&&s[i]==c&i+w?i%4:v)if(!c)for(i=g=m[h+=s[w=v]-47]|h&192|!(v^2|h%8^7&&v|h%8),m[h]=l+=h==a;i<64;m[i++]-=!!m[i])for(g||printf(s+8+3*!!i+!!(i%8),i^a?s[!m[i]]:42);m[a];a=rand()%64);
-}
+int main(){for(char s[99]="0'.7cu^o\e[H\n%c'",*m=s+27,h=52,l=3,v=1,w,c,g=0,i,a=(srand((int)m),4);!g;Sleep(99))for(c=2;c>0;i=3)for(c=kbhit()?getch():1;c--&&++i<8;v=c&&s[i]==c&i+w?i%4:v)if(!c)for(i=g=m[h+=s[w=v]-47]|h&192|!(v^2|h%8^7&&v|h%8),m[h]=l+=h==a;i<64;m[i++]-=!!m[i])for(g||printf(s+8+3*!!i+!!(i%8),i^a?s[!m[i]]:42);m[a];a=rand()%64);}
